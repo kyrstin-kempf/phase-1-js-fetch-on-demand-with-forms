2c29913356af19d1ed9b0373e43808ebd964b3b0
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2022-03-02 19:45:08 -0500

    commit tree 902aeb948697d8b53528478874a661ad07302548

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+
+    inputForm.addEventListener('submit', (handleEvent) => {
+        handleEvent.preventDefault();
+        const input = document.querySelector('input#searchByID');
+        // console.log(handleEvent);
+        
+        console.log(input.value)
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+            // console.log(data);
+        });
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+// event.target.children[1].value
